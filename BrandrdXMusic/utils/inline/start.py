b623925801e4ed7e33ecc4f870eda5cf387c9bd3
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2025-11-19 22:02:41 +0530

    Update start.py

--- BrandrdXMusic/utils/inline/start.py
+++ BrandrdXMusic/utils/inline/start.py
@@ -4,7 +4,7 @@ import config
 from BrandrdXMusic import app
 
 
-def start_pannel(_):
+def start_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
@@ -16,40 +16,11 @@ def start_pannel(_):
             InlineKeyboardButton(text="• ʜᴇʟᴘ •", callback_data="settings_back_helper"),
             InlineKeyboardButton(text="• sᴇᴛ •", callback_data="settings_helper"),
         ],
-
-        # ✅ ADDED API PONG BUTTON HERE
         [
             InlineKeyboardButton(text="💗 API PONG", callback_data="api_pong"),
         ],
-
         [
             InlineKeyboardButton(text="• sᴏᴜꝛᴄᴇ •", url=config.SUPPORT_CHAT),
-        ],
-    ]
-    return buttons
-
-
-
-def private_panel(_):
-    buttons = [
-        [
-            InlineKeyboardButton(
-                text=_["S_B_3"],
-                url=f"https://t.me/{app.username}?startgroup=true",
-            )
-        ],
-        [
-            InlineKeyboardButton(text=_["S_B_5"], url=f"https://t.me/DvisDmBot?start"),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
-        ],
-
-        # ✅ ADDED API PONG BUTTON HERE
-        [
-            InlineKeyboardButton(text="💗 API PONG", callback_data="api_pong"),
-        ],
-
-        [
-            InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
         ],
     ]
     return buttons
